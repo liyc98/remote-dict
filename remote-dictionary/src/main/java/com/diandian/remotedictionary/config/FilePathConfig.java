eac51f7f10a279fabb8a96f770f6776456b8dcbb
yuancheng.li <liyuancheng@meorient.com>
2018-04-04 10:51:07 +0800

    多余引用注释

--- remote-dictionary/src/main/java/com/diandian/remotedictionary/config/FilePathConfig.java
+++ remote-dictionary/src/main/java/com/diandian/remotedictionary/config/FilePathConfig.java
@@ -1,7 +1,6 @@
 package com.diandian.remotedictionary.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
